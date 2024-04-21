485fcf1d714b04a5434ee23403b132d39c3a6dfa
John Doe <johndoe@example.com>
2024-04-21 20:32:11 +0700

    фывфвывф

--- Progect/script_progect.js
+++ Progect/script_progect.js
@@ -15,7 +15,7 @@ new_div.insertAdjacentHTML('afterbegin', newgame_template)
 //сцена 2 карта игры
 
 let newgame = document.getElementById("newgame")
-    map = [0,1,1,1,1,0,1,1,1,1,1,1,0,1,1,1,1,0]
+    map = [0,'pl10.png','pl2.png','pl3.png','pl4.png',0,'blue1.png.160x120r.png','pl5.png','pl6.png','pl7.png','pl8.png','blue1.png.160x120r.png',0,'pl9.png','pl10.png','pl11.png','pl12.png',0]
 newgame.onclick = () => {
     console.log('if выполнился')
     new_div.innerHTML = ''
@@ -36,7 +36,22 @@ function render_map(map) {
         cell = map[i];
         console.log('a')
         cell_obj = document.createElement('div');
-        cell_obj.classList.add('cell')
+        if (map[i] != 0){
+            img_obj1 = document.createElement('img');
+            img_obj1.src = map[i]
+            cell_obj.appendChild(img_obj1)
+            if (i != 16 && i != 4 && i != 11) {
+                img_obj2 = document.createElement('img');
+                img_obj2.src = 'strela.png'
+                cell_obj.appendChild(img_obj2)
+            }
+        }
+        if (i != 0 && i != 5 && i != 12 && i!=17){
+            cell_obj.classList.add('cell')
+            if (i == 6) {
+
+            }
+        }
         
         map_obj.appendChild(cell_obj)
     }
