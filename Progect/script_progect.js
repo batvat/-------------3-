99529de276783f99f20964b80ae5f588d867b06d
E <you@example.com>
2024-05-05 20:26:39 +0700

    asdadas

--- Progect/script_progect.js
+++ Progect/script_progect.js
@@ -20,7 +20,7 @@ new_div.insertAdjacentHTML('afterbegin', newgame_template)
 //сцена 2 карта игры
 
 let newgame = document.getElementById("newgame")
-    map = [0,'map_icon1.png','map_icon1.png','map_icon1.png','map_icon1.png',0,'blue1.png.160x120r.png','map_icon1.png','map_icon1.png','map_icon1.png','map_icon1.png','map_icon1.png',0,'map_icon1.png','map_icon1.png','map_icon1.png','map_icon1.png',0]
+    map = [0,'map_icon1.png','map_icon1.png','map_icon1.png','map_icon1.png',0,'map_icon2.png','map_icon1.png','map_icon1.png','map_icon1.png','map_icon1.png','map_icon1.png',0,'map_icon1.png','map_icon1.png','map_icon1.png','map_icon1.png',0]
 newgame.onclick = () => {
 
     new_div.innerHTML = ''
@@ -31,30 +31,24 @@ newgame.onclick = () => {
 function getRandom(min, max) {
     min = Math.ceil(min);
     max = Math.floor(max);
-    let a = Math.floor(     Math.random() * (max - min) + min); 
+    let a = Math.floor(Math.random() * (max - min) + min); 
     return a 
     
   }
 
-function draw(a1,b1,a2,b2) {
-    console.log(a1,b1,b2,a2)
-    let canvas = document.getElementById('graph');
-    let ctx = canvas.getContext('2d');
-    
-    ctx.beginPath();
-    ctx.moveTo(a1,b1);
-    ctx.lineTo(a2,b2);
-    ctx.strokeStyle = '#1a2edb'
-    ctx.stroke();
-}
+
+  
+
 
 function render_map(map) {
     const canvas_template = `
-    <canvas class="can" id="graph"></canvas>`;
+    <canvas class="can" width="${window.innerWidth}" height="${window.innerHeight}" id="graph"></canvas>`;
     new_div.insertAdjacentHTML('afterbegin', canvas_template)
     const map_template = `
     <div class="map_div" id="map_div"></div>`;
     new_div.insertAdjacentHTML('afterbegin', map_template)
+
+    
 
     const map_obj = document.getElementById('map_div');
     map_div.innerHTML = '';
@@ -64,17 +58,22 @@ function render_map(map) {
 
         cell_obj = document.createElement('div');
         if (map[i] != 0){
+            img_box = document.createElement('div');
+            img_box.classList.add("abs")
+            img_box1 = document.createElement('div');
+            img_box1.classList.add("abs")
             img_obj1 = document.createElement('img');
             img_obj1.src = map[i]
-            img_obj1.classList.add('abs')
-
-            if (Math.random() > 0.5) 
-                {img_obj1.style.top =`${getRandom(0,100)}px`}
-            else 
-                {img_obj1.style.bottom =`${getRandom(0,100)}px`}
-
-                img_obj1.style.right=`${getRandom(0,100)}px`
-                cell_obj.appendChild(img_obj1)
+            img_obj1.classList.add('abs-img')
+            img_obj2 = document.createElement('img');
+            img_obj2.src = 'line1.png'
+            img_obj2.classList.add('abs-img')
+            
+            img_box.appendChild(img_obj1)
+            img_box1.appendChild(img_obj2)
+            
+                cell_obj.appendChild(img_box)
+                cell_obj.appendChild(img_box1)
                 
         }
         if (i != 0 && i != 5 && i != 12 && i!=17){
@@ -83,18 +82,34 @@ function render_map(map) {
 
             }
         }
-        let m = []
         map_obj.appendChild(cell_obj)
-        for (let elem of map_obj.childNodes) {
-            let rect = elem.getBoundingClientRect();
-            let x = rect.left;
-            let y = rect.top;
-                m.push(x)
-                m.push(y)
-            if (m.length == 4) {
-                draw(m[0], m[1], m[2],m[3])
-                m = []
-            }
-        }
     }
+
+
+//     let canvas = document.getElementById('graph');
+//     let ctx = canvas.getContext('2d');
+//     ctx.strokeStyle = 'white'
+//     ctx.lineWidth = '1'
+//     ctx.beginPath();
+
+//     let m = [];
+//     let i = -1;
+//     for (let elem of map_obj.childNodes) {
+//         if (elem.firstChild) {
+//             let rect = elem.firstChild.getBoundingClientRect();
+//             let x = rect.right;
+//             let y = rect.top + 15;
+
+//             m.push([x, y])
+//             i++;
+//             console.log(m)
+//             console.log(m[i])
+//             if (m.length == 1) {
+//                 ctx.moveTo(...m[i]);
+//             } else if (m.length >= 2) {
+//                 ctx.lineTo(...m[i]);
+//             }
+//         }
+//     }
+//     ctx.stroke();
 }
